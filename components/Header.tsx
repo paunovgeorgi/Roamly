a700966dcc5eb7c38c84f8da87f0c909c54d7037
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-05-18 12:26:35 +0300

    Header component updated, trips routes added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
-import { useLocation } from "react-router";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
+import { Link, useLocation } from "react-router";
 import { cn } from "~/lib/utils";
 
 interface Props {
     title: string;
     description: string;
+    ctaText?: string;
+    ctaUrl?: string;
 }
 
-const Header = ( {title, description}: Props) => {
+const Header = ( {title, description, ctaText, ctaUrl}: Props) => {
     const location = useLocation();
 
   return (
@@ -19,6 +22,14 @@ const Header = ( {title, description}: Props) => {
                 location.pathname === '/' ? 'text-base md:text-lg' : 'text-sn md:text-lg '
              )}>{description}</p>
         </article>
+        {ctaText && ctaUrl && (
+          <Link to={ctaUrl}>
+            <ButtonComponent type="button" className="button-class !h-11 !w-full md:w-[240px]">
+              <img src="/assets/icons/plus.svg" alt="plus" className="size-5"/>
+              <span className="p-16-semibold text-white pr-1">{ctaText}</span>
+            </ButtonComponent>
+          </Link>
+        )}
     </header>
   )
 }
